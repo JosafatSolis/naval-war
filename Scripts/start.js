ac4f8cd1d161e8d928c668efa16a9d7054907f5f
JosafatSolis <jsolis@macroaplicaciones.com>
2020-03-24 23:18:18 -0600

    Prueba cambiando Path

--- Scripts/start.js
+++ Scripts/start.js
@@ -6,7 +6,7 @@ function hideInstructions() {
   instructions.style.display = "none";
   var script = document.createElement("script");
   script.setAttribute("type", "text/javascript");
-  script.setAttribute("src", "../Scripts/script.js");
+  script.setAttribute("src", "./Scripts/script.js");
   document.getElementsByTagName("head")[0].appendChild(script);
 }
 
